aec2c078ec187f85def24df20ca0caaca0538dd0
igor90ns <aleksic90@gmail.com>
2018-08-07 18:33:40 +0200

    initial commit, small fixes, basic structure, some animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {trigger, state, transition, style, animate} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate('1s ease-out')
+      ])]),
+      
+      trigger('flyRight',[
+        state('in', style({transform: 'translatex(0)'})),
+        transition('void => *', [
+          style({transform: 'translateX(100%)'}),
+          animate('1s ease-out')
+      ])
+    ]
+      )
+    ]
+    
+
+  
+   
+
+  })
 export class AppComponent {
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Router, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './pro/about/about.component';
+import { SidebarComponent } from './pro/sidebar/sidebar.component';
+import { ProjectsComponent } from './pro/projects/projects.component';
+import { EducationComponent } from './pro/education/education.component';
+
+
+
+const appRoutes:Routes = [
+  {path:'', component:IndexComponent},
+  {path:'home', component:IndexComponent},
+  {path:'about', component:AboutComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'education', component:EducationComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    SidebarComponent,
+    ProjectsComponent,
+    EducationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
